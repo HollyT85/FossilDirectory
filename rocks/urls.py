db014f5059b4b2829fec4afc442b90d5920cdeb0
HollyT85 <h.e.turner@gmail.com>
2023-08-30 09:07:04 +0000

    urls

--- rocks/urls.py
+++ rocks/urls.py
@@ -3,4 +3,5 @@ from rocks import views
 
 urlpatterns = [
     path('rocks/', views.RockList.as_view()),
+    path('rocks/<int:pk>/', views.RockDetail.as_view()),
 ]
--- rocks/views.py
+++ rocks/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Rock
 from .serializers import RockSerializer
+from fossil_api.permissions import IsOwnerOrReadOnly
 
 
 class RockList(APIView):
@@ -28,3 +30,41 @@ class RockList(APIView):
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
         )
+
+def RockDetail(APIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = RockSerializer
+
+    def get_object(self, pk):
+        try:
+            rock = Rock.objects.get(pk=pk)
+            self.check_object_permissions(self.request, rock)
+            return rock
+        except Rock.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        rock = self.get_object(pk)
+        serializer = RockSerializer(
+            rock, context={'request':request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        rock = self.get_object(pk)
+        serializer = RockSerializer(
+            rock, data=request.data, context={'request':request}
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(
+            serializers.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        rock = self.get_object(pk)
+        rock.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
