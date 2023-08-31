854364a2cbb472fe3fdf460f3f6816d7355ee43e
HollyT85 <h.e.turner@gmail.com>
2023-08-31 12:59:30 +0000

    addedc omment filter to rock posts

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics, permissions
 from fossil_api.permissions import IsOwnerOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import RockComments
 from .serializers import RockCommentSerializer, RockCommentDetailSerializer
 
@@ -9,6 +10,9 @@ class RockCommentList(generics.ListCreateAPIView):
     serializer_class = RockCommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = RockComments.objects.all()
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['rock_post']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
