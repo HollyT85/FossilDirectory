4384b6fa6c8d0212a28713a0dbfc6e549a97e442
HollyT85 <h.e.turner@gmail.com>
2023-08-31 12:45:57 +0000

    updated rock views and added filters for searching and sorting

--- rocks/views.py
+++ rocks/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from rest_framework import status, permissions
+from rest_framework import filters, permissions, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Rock
@@ -7,64 +7,30 @@ from .serializers import RockSerializer
 from fossil_api.permissions import IsOwnerOrReadOnly
 
 
-class RockList(APIView):
+class RockList(generics.ListCreateAPIView):
+
     serializer_class = RockSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
 
-    def get(self, request):
-        rocks = Rock.objects.all()
-        serializer = RockSerializer(
-            rocks, many=True, context={'request':request}
-        )
-        return Response(serializer.data)
+    queryset = Rock.objects.all()
 
-    def post(self, request):
-        serializer = RockSerializer(
-            data=request.data, context={'request':request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
 
-class RockDetail(APIView):
-    permission_classes = [IsOwnerOrReadOnly]
+    search_fields = [
+        'title',
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class RockDetail(generics.RetrieveUpdateDestroyAPIView):
+
     serializer_class = RockSerializer
-
-    def get_object(self, pk):
-        try:
-            rock = Rock.objects.get(pk=pk)
-            self.check_object_permissions(self.request, rock)
-            return rock
-        except Rock.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        rock = self.get_object(pk)
-        serializer = RockSerializer(
-            rock, context={'request':request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        rock = self.get_object(pk)
-        serializer = RockSerializer(
-            rock, data=request.data, context={'request':request}
-        )
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-
-        return Response(
-            serializers.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, pk):
-        rock = self.get_object(pk)
-        rock.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Rock.objects.all()
